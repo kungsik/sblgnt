ba592cf4422c2897ccd9d7e0d8792fc0e39cba54
alphalef <kungsik@gmail.com>
2019-08-07 17:59:10 +0900

    first commit

---
+++ otext.tf
@@ -0,0 +1,9 @@
+@config
+@fmt:lex-orig-full={UnicodeLemma} 
+@fmt:text-orig-full={g_word}{trailer}
+@fmt:text-orig-plain={g_plain}{trailer}
+@sectionFeatures=book,chapter,verse
+@sectionTypes=book,chapter,verse
+@writtenBy=Text-Fabric
+@dateWritten=2017-03-21T14:56:06Z
+
